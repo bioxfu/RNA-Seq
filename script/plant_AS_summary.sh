17ddb30d7a855f1acacdb87569b76004c3c4e332
Fu Xing <bio.xfu@gmail.com>
2018-03-07 13:46:50 +0800

    update plant_AS_summary

--- script/plant_AS_summary.sh
+++ script/plant_AS_summary.sh
@@ -1,11 +1,15 @@
-echo -e "AS_event\tgene\tpvalue\tAS_type" > table/AS_summary.tsv
-grep -v 'pvalue' AS/all_sample_A3SS_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tA3SS"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary.tsv
-grep -v 'pvalue' AS/all_sample_A5SS_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tA5SS"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary.tsv 
-grep -v 'pvalue' AS/all_sample_AFE_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tAFE"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary.tsv
-grep -v 'pvalue' AS/all_sample_ALE_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tALE"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary.tsv
-grep -v 'pvalue' AS/all_sample_MXE_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tMXE"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary.tsv
-grep -v 'pvalue' AS/all_sample_RI_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tRI"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary.tsv
-grep -v 'pvalue' AS/all_sample_SE_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tSE"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary.tsv
+#VS=var2-9_vs_svr75
+#VS=WT_vs_var2-T-DNA
+VS=WT_vs_var2-9
 
-Rscript script/plant_AS_anno.R config.yaml table/AS_summary.tsv table/AS_summary_anno.tsv
+echo -e "AS_event\tgene\tpvalue\tAS_type" > table/AS_summary_${VS}.tsv
+grep -v 'pvalue' AS/all_sample_A3SS_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tA3SS"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary_${VS}.tsv
+grep -v 'pvalue' AS/all_sample_A5SS_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tA5SS"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary_${VS}.tsv 
+grep -v 'pvalue' AS/all_sample_AFE_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tAFE"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary_${VS}.tsv
+grep -v 'pvalue' AS/all_sample_ALE_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tALE"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary_${VS}.tsv
+grep -v 'pvalue' AS/all_sample_MXE_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tMXE"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary_${VS}.tsv
+grep -v 'pvalue' AS/all_sample_RI_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tRI"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary_${VS}.tsv
+grep -v 'pvalue' AS/all_sample_SE_fisher_test.tsv|awk '{if($2<0.05)print $1"\t"$2"\tSE"}'|sed -r 's/\.[0-9]\t/\t/'|sed 's/|AT/\tAT/' >> table/AS_summary_${VS}.tsv
+
+Rscript script/plant_AS_anno.R config.yaml table/AS_summary_${VS}.tsv table/AS_summary_anno_${VS}.tsv
 
