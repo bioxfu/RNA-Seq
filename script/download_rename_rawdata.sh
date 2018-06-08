2207e34920415f7b883df39a82cca19ad1e1dae0
Fu Xing <bio.xfu@gmail.com>
2018-06-08 15:19:34 +0800

    use rsync to download data

--- script/download_rename_rawdata.sh
+++ script/download_rename_rawdata.sh
@@ -1,5 +1,9 @@
 DIR=$1
 mkdir fastq
-scp -r xfu@10.41.25.100:$DIR ./fastq
-ls ./fastq/*/*/* |./script/rush -k 'mv {} fastq/{/%}_{%@.+_(R\d).fq.gz}.fastq.gz' --dry-run
-ls ./fastq/*/*/* |./script/rush -k 'mv {} fastq/{/%}_{%@.+_(R\d).fq.gz}.fastq.gz'
+
+host=xfu@10.41.25.100
+rsync -var $host:$DIR ./fastq/
+
+ls ./fastq/*/*/* |./script/rush -k 'mv {} fastq/s{/%}_{%@.+[_.](R\d).fa*s*t*q.gz}.fastq.gz' --dry-run
+
+ls ./fastq/*/*/* |./script/rush -k 'mv {} fastq/s{/%}_{%@.+[_.](R\d).fa*s*t*q.gz}.fastq.gz' 
