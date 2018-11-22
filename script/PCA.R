95f5a5a438bace57228883121aa16ad629872c94
Fu Xing <bio.xfu@gmail.com>
2018-11-22 16:03:48 +0800

    update PCA plot

--- script/PCA.R
+++ script/PCA.R
@@ -31,16 +31,30 @@ if (grepl('+', config$seq_info$replicate)) {
 }
 
 pdf(pca_output, hei=7, wid=7)
+# plot using sample labels (for quality control)
 layout(matrix(c(1,2),nrow=1), wid=c(5, 2))
-par(mar=c(5,4,4,0))
+par(mar=c(5,5,4,0))
 par(xpd=TRUE)
-plot(x[,1], x[,2], xlim=range(x[,1])*1.1, ylim=range(x[,2])*1.1, col=cols, cex=1, type='n',
+plot(x[,1], x[,2], xlim=range(x[,1])*1.1, ylim=range(x[,2])*1.1, 
+     col=cols, type='n', cex.axis=1.5, cex.lab=1.5,
      xlab=paste0('PC1 (',prop_var[1],'% of Variance)'),
      ylab=paste0('PC2 (',prop_var[2],'% of Variance)')
 )
 text(x[,1], x[,2], colnames(cpm), col=cols)
 par(mar=c(5,0,4,0))
 plot.new()
-legend('left', unique(config$groups), pch = 1, col=set2_cols, bty='n')
-dev.off()
+legend('left', unique(config$groups), pch = 16, col=set2_cols, bty='n', cex=1.5)
 
+#plot using solid points (for publication)
+layout(matrix(c(1,2),nrow=1), wid=c(5, 2))
+par(mar=c(5,5,4,0))
+par(xpd=TRUE)
+plot(x[,1], x[,2], xlim=range(x[,1])*1.1, ylim=range(x[,2])*1.1, 
+     col=cols, cex=1.5, pch= 16, cex.axis=1.5, cex.lab=1.5,
+     xlab=paste0('PC1 (',prop_var[1],'% of Variance)'),
+     ylab=paste0('PC2 (',prop_var[2],'% of Variance)')
+)
+par(mar=c(5,0,4,0))
+plot.new()
+legend('left', unique(config$groups), pch = 16, col=set2_cols, bty='n', cex=1.5)
+dev.off()
