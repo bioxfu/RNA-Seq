09e800f86654d06cd489c5bb0a3e9e0ced150b3f
Fu Xing <bio.xfu@gmail.com>
2018-11-23 14:37:18 +0800

    do not export env

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 source activate gmatic
-conda env export > doc/environment.yml
+#conda env export > doc/environment.yml
 
 if [ ! -d fastqc ]; then
 	mkdir -p fastq fastqc/raw fastqc/clean clean bam count track table stat figure RData AS
